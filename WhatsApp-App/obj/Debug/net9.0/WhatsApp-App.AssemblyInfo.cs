f0503043a8a6f1772735a2481898a70e331a2480
Alex Purser <AP276620@falmouth.ac.uk>
2025-05-28 21:43:35 +0100

    Added some message filtering.
    Added participant list, top 10 words, chattiest. Need to add some more and put some visuals in instead.

--- WhatsApp-App/obj/Debug/net9.0/WhatsApp-App.AssemblyInfo.cs
+++ WhatsApp-App/obj/Debug/net9.0/WhatsApp-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhatsApp-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fcecaddbfe89f7ffa18817b4064a0faa87a5b7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7417ca6b6df2a6206e9e1e2fa4cb8cde44885ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhatsApp-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhatsApp-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
