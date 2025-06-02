da2b2228d1e1973fa2f4e031211ebea7951e2537
Alex Purser <AP276620@falmouth.ac.uk>
2025-06-02 12:53:56 +0100

    Improved navigation and added some documentation.

--- WhatsApp-App/obj/Debug/net9.0/WhatsApp-App.AssemblyInfo.cs
+++ WhatsApp-App/obj/Debug/net9.0/WhatsApp-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhatsApp-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd8ec222a78dfe38f69a39efb222defe5dfdbae9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c8796d8c09d5c2037da3c6cdc3bfb6b84cad38")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhatsApp-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhatsApp-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
